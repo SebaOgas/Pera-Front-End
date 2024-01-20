684aa2fd26c2568ed09358a132b9aca3442b5937
SebaOgas <saoga07.sao@gmail.com>
2024-01-20 12:27:54 -0300

    Proyecto inicializado
    SvelteKit inicializado con typescript y bootstrap 5.
    Estilos básicos creados.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Pera-Front-End
-Repositorio de Front-End para proyecto "Pera" del Seminario Integrador 2024 de Ingeniería en Sistemas de Información
+# Repositorio de Front-End para proyecto "Pera" del Seminario Integrador 2024 de Ingeniería en Sistemas de Información
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div class="container-fluid bg-darker text-light text-bigger text-bold">
+    Welcome to SvelteKit
+</div>
+
+
+<div class="container-fluid bg-light text-darker text-medium">
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</div>
+
+
+
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+ 
+    onMount(() => {
+        console.log("Hola mundo");
+    })
+</script>
