29ef4b606dfd8c86b2f368e5c7c14a9c7ac9b427
SebaOgas <saoga07.sao@gmail.com>
2024-03-19 18:41:23 -0300

    Minor bugfixes

--- src/routes/(rutas)/CrearBanco/ServicioCrearBanco.ts
+++ src/routes/(rutas)/CrearBanco/ServicioCrearBanco.ts
@@ -5,12 +5,13 @@ const BASE_URL = "http://localhost:8080";
 export const ServicioCrearBanco = {
     
     crear: async (dto: DTOCrearBanco) : Promise<string> => {
-        const response = await fetch(`${BASE_URL}/Banco/crearBanco`, {
+        const response = await fetch(`${BASE_URL}/CrearBanco/crear`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             mode: 'cors',
+            credentials: "include",
             body: JSON.stringify(dto)
         });
         const data = await response.text();
